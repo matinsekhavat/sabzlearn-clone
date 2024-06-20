eb261db1a9cb4859ef81902ce316d126f06bc146
matinsekhavat <matinhyphen@gmail.com>
2024-06-20 13:20:09 +0330

    complete v1.0 of show for more courses

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,26 @@
 import HeroLanding from "../ui/HeroLanding";
+import { FaLongArrowAltLeft } from "react-icons/fa";
 
 function HomePage() {
-  return <>{<HeroLanding />}</>;
+  return (
+    <>
+      <HeroLanding />
+      <div className="container my-2">
+        <div className="flex flex-col items-center gap-y-4 sm:flex-row sm:justify-between">
+          <div className="text-center">
+            <h3 className="text-xl font-semibold mb-2 ">
+              آخرین دوره های سبزلرن
+            </h3>
+            <span className="text-gray-400">سکوی پرتاپ شما به سمت موفقیت</span>
+          </div>
+          <div className="flex items-center gap-2 text-lg text-green-500">
+            <p>مشاهده همه دوره ها</p>
+            <FaLongArrowAltLeft className="mt-1" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default HomePage;
