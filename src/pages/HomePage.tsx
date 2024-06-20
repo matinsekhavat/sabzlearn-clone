d374343806e8b4a167e022f23b3eefeae6b61982
matinsekhavat <matinhyphen@gmail.com>
2024-06-20 13:21:30 +0330

    some changes and add comments in show for last courses section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import HeroLanding from "../ui/HeroLanding";
 import { FaLongArrowAltLeft } from "react-icons/fa";
 
@@ -5,16 +6,20 @@ function HomePage() {
   return (
     <>
       <HeroLanding />
+      {/* Show Last courses
+       */}
       <div className="container my-2">
         <div className="flex flex-col items-center gap-y-4 sm:flex-row sm:justify-between">
+          {/* text for last courses */}
           <div className="text-center">
             <h3 className="text-xl font-semibold mb-2 ">
               آخرین دوره های سبزلرن
             </h3>
             <span className="text-gray-400">سکوی پرتاپ شما به سمت موفقیت</span>
           </div>
+          {/* Links */}
           <div className="flex items-center gap-2 text-lg text-green-500">
-            <p>مشاهده همه دوره ها</p>
+            <Link to="/">مشاهده همه دوره ها</Link>
             <FaLongArrowAltLeft className="mt-1" />
           </div>
         </div>
