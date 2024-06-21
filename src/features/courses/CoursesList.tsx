81a1c00b28defe823535c2de43ae6b014ac50151
matinsekhavat <matinhyphen@gmail.com>
2024-06-21 23:42:25 +0330

    make CoursesList more flexible and also import swiper/css file

--- src/features/courses/CoursesList.tsx
+++ src/features/courses/CoursesList.tsx
@@ -1,7 +1,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import CourseItem from "./CourseItem";
 import { Pagination } from "swiper/modules";
-
+import "swiper/css";
 export type courseProps = {
   background_url: string;
   title: string;
@@ -18,52 +18,53 @@ type courseListProps = {
 };
 
 function CoursesList({ courses, isCarousel }: courseListProps) {
-  if (!isCarousel)
-    return (
-      <div className="container my-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-7">
-        <CourseItem />
-        <CourseItem />
-        <CourseItem />
-      </div>
-    );
   if (isCarousel) {
     return (
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <CourseItem />
-        </SwiperSlide>
-        <SwiperSlide>
-          <CourseItem />
-        </SwiperSlide>
-        <SwiperSlide>
-          <CourseItem />
-        </SwiperSlide>
-        <SwiperSlide>
-          <CourseItem />
-        </SwiperSlide>
-        <SwiperSlide>
-          <CourseItem />
-        </SwiperSlide>
-        <SwiperSlide>
-          <CourseItem />
-        </SwiperSlide>
-        <SwiperSlide>
-          <CourseItem />
-        </SwiperSlide>
-        <SwiperSlide>
-          <CourseItem />
-        </SwiperSlide>
-      </Swiper>
+      <div className="container">
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <CourseItem />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CourseItem />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CourseItem />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CourseItem />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CourseItem />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CourseItem />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CourseItem />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CourseItem />
+          </SwiperSlide>
+        </Swiper>
+      </div>
     );
   }
+  return (
+    <div className="container my-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-7">
+      <CourseItem />
+      <CourseItem />
+      <CourseItem />
+    </div>
+  );
 }
 
 export default CoursesList;
