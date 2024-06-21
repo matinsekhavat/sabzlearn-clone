4c8e741ed6a16da05c710ac8432846dc5fdb8b6b
matinsekhavat <matinhyphen@gmail.com>
2024-06-21 17:50:37 +0330

    make HeadTitle component more dynamic

--- src/ui/HeadTitle.tsx
+++ src/ui/HeadTitle.tsx
@@ -3,8 +3,10 @@ import { ReactNode } from "react";
 type headTitleProps = {
   children?: ReactNode;
   color: string;
+  title: string;
+  subTitle: string;
 };
-function HeadTitle({ children, color }: headTitleProps) {
+function HeadTitle({ children, color, title, subTitle }: headTitleProps) {
   return (
     <div className="container my-2">
       <div className="flex flex-col items-center gap-y-4 sm:flex-row sm:justify-between">
@@ -13,13 +15,11 @@ function HeadTitle({ children, color }: headTitleProps) {
           <div className="flex  items-center  gap-2">
             <div
               style={{ backgroundColor: `${color}` }}
-              className="size-6  rounded-md hidden sm:block"
+              className="size-5  rounded-md hidden sm:block"
             ></div>
-            <h3 className="text-xl font-semibold  ">آخرین دوره های سبزلرن</h3>
+            <h3 className="text-xl font-semibold  ">{title}</h3>
           </div>
-          <span className="text-gray-400 mt-2">
-            سکوی پرتاپ شما به سمت موفقیت
-          </span>
+          <span className="text-gray-400 mt-2">{subTitle}</span>
         </div>
         {/* Links */}
         <div className="flex items-center gap-2 text-lg text-green-500">
