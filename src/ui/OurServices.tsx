77ba5775d1ad92e528ed3087880262a9dd58c543
matinsekhavat <matinhyphen@gmail.com>
2024-06-22 00:44:23 +0330

    make ourServices component flexible + props type

--- src/ui/OurServices.tsx
+++ src/ui/OurServices.tsx
@@ -1,20 +1,32 @@
 import { BiBook } from "react-icons/bi";
 
-function OurServices() {
+type OurServicesProps = {
+  title: string;
+  description: string;
+  icon: JSX.Element;
+  iconColor: string;
+  iconBgInstance: string;
+};
+
+function OurServices({
+  title,
+  description,
+  icon,
+  iconColor,
+  iconBgInstance,
+}: OurServicesProps) {
   return (
-    <div className="p-4 rounded-2xl flex flex-col gap-2 items-center bg-white lg:flex-row lg:py-8">
+    <div className="p-4 rounded-2xl flex flex-col gap-2 items-center bg-white lg:flex-row lg:py-8 ">
       <div className="flex flex-col items-center lg:flex-row">
-        <div className="h-12 bg-slate-200 w-20 rounded-2xl lg:w-12 lg:h-20"></div>
-        <BiBook className="text-[2.5rem] text-blue-600 -mt-4 lg:mt-0 lg:-mr-6" />
+        <div
+          style={{ backgroundColor: `${iconBgInstance}` }}
+          className="h-12 bg-slate-200 w-20 rounded-2xl lg:w-12 lg:h-20"
+        ></div>
+        {icon}
       </div>
       <div className="text-center mt-2">
-        <h1 className="text-slate-800 font-semibold lg:text-xl">
-          تضمین کاملترین محتوا
-        </h1>
-        <p className="text-sm mt-2 text-stone-500 lg:text-base">
-          بزار خیالت راحت کنم توی دوره هامون به ریز ترین موارد پرداختیم بعداز
-          دیدن این دوره نیاز به هیچ آموزش دیگه ای نداری
-        </p>
+        <h1 className="text-slate-800 font-semibold lg:text-xl">{title}</h1>
+        <p className="text-sm mt-2 text-stone-500 lg:text-sm">{description}</p>
       </div>
     </div>
   );
