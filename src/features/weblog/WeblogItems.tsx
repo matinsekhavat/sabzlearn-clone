80c4bfb54dd237f2b2d704b88e4db27b20d8badb
matinsekhavat <matinhyphen@gmail.com>
2024-06-24 00:57:13 +0330

    implememt weblogItems UI

--- src/features/weblog/WeblogItems.tsx
+++ src/features/weblog/WeblogItems.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+import { BiArrowToLeft, BiCalendar } from "react-icons/bi";
+import { FaUser } from "react-icons/fa";
 
 function WeblogItems() {
-  return <div>WeblogItems</div>;
+  return (
+    <div className="p-4 bg-white rounded-xl flex flex-col gap-8">
+      <div className="relative h-[182px]">
+        <img
+          src="https://sabzlearn.ir/wp-content/uploads/2024/05/1.1-1-768x427.webp"
+          alt="sabzlearn webloag banner"
+          className="h-full object-cover"
+        />
+        <div className="banner__opacity"></div>
+      </div>
+      {/* middle section */}
+      <div>
+        <h2 className="line-clamp-2 text-slate-800">
+          پایگاه داده یا دیتابیس چیست؟ از انواع تا کاربرد دیتابیس
+        </h2>
+        <p className="line-clamp-4 text-sm text-stone-500 my-4">
+          دیتابیس چیست؟ دیتابیس، فقط یک سری از جداول و رکوردهای خشک و بی‌روح
+          نیست. بلکه یک دیدگاه جامع و یک نگرش زنده به مدیریت و ذخیره‌سازی
+          داده‌هاست. در این مقاله…
+        </p>
+        {/* Author and Date - group */}
+        <div className="flex items-center justify-between flex-wrap text-stone-500 text-sm">
+          <div className="flex items-center gap-1">
+            <FaUser />
+            <span>متین سخاوت</span>
+          </div>
+          <div className="flex items-center gap-2 ">
+            <span className="text-base">
+              <BiCalendar />
+            </span>
+            <span>1403/03/30</span>
+          </div>
+        </div>
+      </div>
+      {/* weblog footer */}
+      <div className="border-t border-stone-300 text-green-700 ">
+        <div className="text-center flex items-center justify-center pt-4 gap-2">
+          <span>مطالعه مقاله</span>
+          <span>
+            {" "}
+            <BiArrowToLeft />{" "}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default WeblogItems;
