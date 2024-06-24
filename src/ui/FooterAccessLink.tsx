5c90904e170b32937acf82067b989fef39780467
matinsekhavat <matinhyphen@gmail.com>
2024-06-24 19:33:14 +0330

    strict type mode in footer component

--- src/ui/FooterAccessLink.tsx
+++ src/ui/FooterAccessLink.tsx
@@ -1,18 +1,49 @@
 import { Link } from "react-router-dom";
 
-type FooterProps = {
-  headTitle: string;
-  accessLink: { title: string; path: "/" }[];
+type AccessLinkBase = {
+  title: string;
+  path: string;
 };
 
-function FooterAccessLink({ headTitle, accessLink }: FooterProps) {
+type AccessLinkWithoutIcon = AccessLinkBase;
+
+type AccessLinkWithIcon = AccessLinkBase & {
+  icons: JSX.Element;
+};
+
+type FooterBaseProps = {
+  headTitle: string;
+};
+
+type FooterWithoutIconsProps = FooterBaseProps & {
+  accessLink: AccessLinkWithoutIcon[];
+  hasIcon?: false;
+};
+
+type FooterWithIconsProps = FooterBaseProps & {
+  accessLink: AccessLinkWithIcon[];
+  hasIcon: true;
+};
+
+type FooterProps = FooterWithoutIconsProps | FooterWithIconsProps;
+
+function FooterAccessLink({ headTitle, accessLink, hasIcon }: FooterProps) {
   return (
-    <div>
-      <p className="my-3">{headTitle} </p>
-      <ul className="space-y-4">
+    <div className="">
+      <p className="my-3 text-slate-800 font-semibold">{headTitle}</p>
+      <ul className="space-y-4 ">
         {accessLink.map((item) => (
           <Link to={item.path} key={item.title} className="block">
-            <li className="text-sm text-stone-500">{item.title}</li>
+            <li
+              className={`text-sm sm:text-base text-stone-500 ${
+                hasIcon ? "flex items-center gap-2" : ""
+              }`}
+            >
+              {hasIcon && "icons" in item && item.icons && (
+                <span>{item.icons}</span>
+              )}
+              <span>{item.title}</span>
+            </li>
           </Link>
         ))}
       </ul>
