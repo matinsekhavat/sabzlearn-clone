9e10c63fc439f077206d2938615ad178511c71b5
matinsekhavat <matinhyphen@gmail.com>
2024-06-24 20:50:43 +0330

    define /courses path in react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Applayout from "./ui/Applayout";
 import HomePage from "./pages/HomePage";
+import Courses from "./pages/Courses";
 
 function App() {
   return (
     <Routes>
       <Route element={<Applayout />}>
         <Route path="/" element={<HomePage />} />
+        <Route path="/courses" element={<Courses />} />
       </Route>
     </Routes>
   );
