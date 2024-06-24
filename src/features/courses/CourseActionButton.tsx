0f4cd7ce46ce66cb3077056700496b9d3ddc5051
matinsekhavat <matinhyphen@gmail.com>
2024-06-25 02:29:39 +0330

    create and implement CourseActionButton logic  next step is ui

---
+++ src/features/courses/CourseActionButton.tsx
@@ -0,0 +1,43 @@
+import { ReactNode, useRef } from "react";
+import Overlay from "../../ui/Overlay";
+import useDetectOutsideClick from "../../hooks/useOutsideClick";
+
+type CourseActionButtonProps = {
+  children: ReactNode;
+  openHandler: () => void;
+  isOpen: boolean;
+  closeHandler: () => void;
+};
+
+function CourseActionButton({
+  children,
+  isOpen,
+  openHandler,
+  closeHandler,
+}: CourseActionButtonProps) {
+  const dropMenu = useRef<HTMLDivElement>(null);
+  useDetectOutsideClick(dropMenu, closeHandler, true);
+  return (
+    <>
+      <button
+        className="bg-white rounded-xl text-center py-4 block w-full"
+        onClick={openHandler}
+      >
+        {children}
+      </button>
+      {isOpen ? (
+        <div
+          className="fixed bottom-0 left-0 right-0 h-96 bg-white rounded-t-md p-x4 py-2 z-50"
+          ref={dropMenu}
+        >
+          dummy data
+        </div>
+      ) : (
+        ""
+      )}
+      {<Overlay isShow={isOpen} />}
+    </>
+  );
+}
+
+export default CourseActionButton;
