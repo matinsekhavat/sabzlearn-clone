84f6cecedeaf02bbca63b36e6bbc008c124bd6f6
matinsekhavat <matinhyphen@gmail.com>
2024-06-25 02:30:26 +0330

    import and use CourseFilter component into main Courses.tsx component file

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import CourseFilter from "../features/courses/CourseFilter";
+import HeadTitle from "../ui/HeadTitle";
+import { e2p } from "../utils/replaceNumber";
 
 function Courses() {
-  return <div>Courses</div>;
+  return (
+    <div className="container">
+      <HeadTitle
+        title="دوره ها"
+        subTitle="همه دوره ها در یک نگاه"
+        color="yellow"
+      >
+        {e2p("44")}عنوان اموزشی
+      </HeadTitle>
+      <CourseFilter />
+    </div>
+  );
 }
 
 export default Courses;
