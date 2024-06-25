c181a0fc0e97628c3898d9cb97c43376c85d8e75
matinsekhavat <matinhyphen@gmail.com>
2024-06-25 14:36:07 +0330

    fix empty {} warn

--- src/features/courses/DropMenu.tsx
+++ src/features/courses/DropMenu.tsx
@@ -1,7 +1,7 @@
 import React, { forwardRef } from "react";
 import DropMenuskeleton from "./DropMenuskeleton";
 
-const DropMenu = forwardRef<HTMLDivElement, {}>(({}, ref) => {
+const DropMenu = forwardRef<HTMLDivElement, {}>((props, ref) => {
   return (
     <div
       className="fixed bottom-0 left-0 overflow-hidden right-0 h-96 bg-white rounded-t-md p-x4 pb-2 z-50"
