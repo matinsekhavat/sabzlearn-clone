bfd6ee17a46308b6b3646d9d272ab7a750ccece8
matinsekhavat <matinhyphen@gmail.com>
2024-06-25 14:16:16 +0330

    wrapp filterContextProvider into /courses path

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 import Applayout from "./ui/Applayout";
 import HomePage from "./pages/HomePage";
+
 import Courses from "./pages/Courses";
+import FilterProvider from "./context/FilterContext";
 
 function App() {
   return (
     <Routes>
       <Route element={<Applayout />}>
         <Route path="/" element={<HomePage />} />
-        <Route path="/courses" element={<Courses />} />
+        <Route
+          path="/courses"
+          element={
+            <FilterProvider>
+              <Courses />
+            </FilterProvider>
+          }
+        />
       </Route>
     </Routes>
   );
