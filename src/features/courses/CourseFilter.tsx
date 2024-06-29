5fdc3edd27e4579d2c6e02a12c78010dd46ca6c0
matinsekhavat <matinhyphen@gmail.com>
2024-06-29 14:38:58 +0330

    fix some bugs

--- src/features/courses/CourseFilter.tsx
+++ src/features/courses/CourseFilter.tsx
@@ -1,6 +1,8 @@
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import CourseActionButton from "./CourseActionButton";
 import { useFilter } from "../../context/FilterContext";
+import { useRef } from "react";
+import { useSearchParams } from "react-router-dom";
 
 function CourseFilter() {
   const {
@@ -11,6 +13,17 @@ function CourseFilter() {
     courseMenuHandler,
     filterMenuHandler,
   } = useFilter();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  function handleSearchInputToUrl() {
+    if (!inputRef.current?.value.trim()) {
+      searchParams.delete("query");
+    }
+
+    searchParams.set("query", inputRef.current!.value);
+    setSearchParams(searchParams);
+  }
   return (
     // mobile design
     <div>
@@ -20,20 +33,17 @@ function CourseFilter() {
           type="text"
           placeholder="جستجو بین دوره ها"
           className="w-full p-2 flex-1 rounded-md border-none outline-none"
+          ref={inputRef}
         />
-        <button className="flex items-center justify-center rounded-md ml-2">
+        <button
+          className="flex items-center justify-center rounded-md ml-2"
+          onClick={handleSearchInputToUrl}
+        >
           <FaMagnifyingGlass className="text-2xl size-full p-1" />
         </button>
       </div>
       {/* action buttons */}
-      <div className="grid grid-cols-2 gap-4 my-2">
-        <CourseActionButton
-          openHandler={filterMenuHandler}
-          isOpen={isFilterVisible}
-          closeHandler={closeFilterMenuHander}
-        >
-          فیلتر
-        </CourseActionButton>
+      <div className="grid grid-cols-1 gap-4 my-2">
         <CourseActionButton
           openHandler={courseMenuHandler}
           isOpen={isCourseVisible}
@@ -47,3 +57,4 @@ function CourseFilter() {
 }
 
 export default CourseFilter;
+
