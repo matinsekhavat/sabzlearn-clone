94d2eb53bbcea997b199d686539dbc2fa4d2287a
matinsekhavat <matinhyphen@gmail.com>
2024-07-07 18:58:25 +0330

    make CourseExtraInfo more complex with type structure

--- src/features/courses/SingleCourseDiff/CourseExtraInfo.tsx
+++ src/features/courses/SingleCourseDiff/CourseExtraInfo.tsx
@@ -1,14 +1,22 @@
 import { FaClock } from "react-icons/fa";
 
-function CourseExtraInfo() {
+interface CourseExtraInfoProps {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+}
+
+function CourseExtraInfo({
+  icon = <FaClock />,
+  title,
+  description,
+}: CourseExtraInfoProps) {
   return (
-    <div className="flex flex-row items-center gap-4 bg-white rounded-xl p-3">
-      <span className="text-green-500 text-3xl">
-        <FaClock />
-      </span>
-      <div className="space-y-1">
-        <p className="text-lg">وضعیت دوره</p>
-        <p className="text-sm text-stone-400">99 ساعت</p>
+    <div className="flex flex-row items-center gap-4 bg-white rounded-xl p-3 md:p-4 lg:p-5">
+      <span className="text-green-500 text-[42px]  ">{icon}</span>
+      <div className="space-y-1 md:space-y-2 lg:space-y-3">
+        <p className="text-base font-semibold ">{title}</p>
+        <p className="text-sm text-stone-500 ">{description}</p>
       </div>
     </div>
   );
